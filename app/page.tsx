8a3d259123bb50eb2823a7a167a109dd769bc7d9
0yvz <oyvindg@live.no>
2023-09-26 21:54:20 +0200

    Imported 3d model

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+"use client";
+
+import Spline from "@splinetool/react-spline";
+import React from "react";
+import "./globals.css";
+import type { Metadata } from "next";
+
+export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="w-screen h-screen">
+      <Spline scene="https://prod.spline.design/n66COpNj2gqSKfU1/scene.splinecode" />
+    </div>
   );
 }
